3c557fb559a0697bd256fc024a323b332c6b6f40
Whitney <whitney.atonya@strathmore.edu>
2023-02-19 09:58:10 +0300

    Spinner Added

--- src/component/Subscription.jsx
+++ src/component/Subscription.jsx
@@ -1,6 +1,54 @@
 import React from 'react';
 import { FacebookShareButton, WhatsappShareButton, TelegramShareButton, TwitterShareButton } from 'react-share';
 import { FacebookIcon, WhatsappIcon, TelegramIcon, TwitterIcon } from 'react-share';
+const Subscription = () => {
+
+  return (
+    <div class="card">
+    <img src="/assets/bg5.jpg" class="rounded mx-auto d-block" alt="background"></img>
+    <div class="d-flex justify-content-center">
+        <div class="spinner-border" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </div>
+      <div class="d-flex justify-content-center">
+      <p>Kindly wait as your subscription is being processed</p>
+      </div>
+      <div class="d-flex justify-content-center">
+        <FacebookShareButton
+          url="https://skiza.africomltd.com/"
+          quote={"Enhance Your Calls with Skiza - The Music Caller Tune Subscription Service"}
+        >
+          <FacebookIcon logoFillColor="white" round={true}></FacebookIcon>
+        </FacebookShareButton>
+        <WhatsappShareButton
+          title={"Enhance Your Calls with Skiza - The Music Caller Tune Subscription Service"}
+          url="https://skiza.africomltd.com/"
+        >
+          <WhatsappIcon logoFillColor="white" round={true}></WhatsappIcon>
+        </WhatsappShareButton>
+        <TelegramShareButton
+          title={"Enhance Your Calls with Skiza - The Music Caller Tune Subscription Service"}
+          url="https://skiza.africomltd.com/"
+        >
+          <TelegramIcon logoFillColor="white" round={true}></TelegramIcon>
+        </TelegramShareButton>
+        <TwitterShareButton
+          title={"Enhance Your Calls with Skiza - The Music Caller Tune Subscription Service"}
+          url="https://skiza.africomltd.com/"
+        >
+          <TwitterIcon logoFillColor="white" round={true}></TwitterIcon>
+        </TwitterShareButton>
+      </div>
+      </div>
+    );
+  };
+  
+  export default Subscription
+
+/*import React from 'react';
+import { FacebookShareButton, WhatsappShareButton, TelegramShareButton, TwitterShareButton } from 'react-share';
+import { FacebookIcon, WhatsappIcon, TelegramIcon, TwitterIcon } from 'react-share';
 
 const Subscription = () => {
 
@@ -46,4 +94,4 @@ const Subscription = () => {
   );
 };
 
-export default Subscription
+export default Subscription*/
