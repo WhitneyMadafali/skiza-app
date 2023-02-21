011ed73dba1e290a68287ffaf95ddbe59e37daf0
Whitney Madafali <65074238+WhitneyMadafali@users.noreply.github.com>
2023-02-21 16:00:32 +0300

    Pagination Added

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -7,6 +7,8 @@ const Products = () => {
     const [data, setData] = useState([]);
     const [filter, setFilter] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage, setItemsPerPage] = useState(8);
     let componentMounted = true;
 
     useEffect(() => {
@@ -17,7 +19,6 @@ const Products = () => {
                 setData(await response.clone().json());
                 setFilter(await response.json());
                 setLoading(false);
-                console.log(filter)
             }
             return () => {
                 componentMounted = false;
@@ -26,48 +27,68 @@ const Products = () => {
         getProducts();
     }, []);
 
+
     const Loading = () => {
         return (
-           <>
+            <>
                 <div className="col-md-3">
-                    <Skeleton height={350}/>
+                    <Skeleton height={350} />
                 </div>
-           </>
+            </>
         );
     };
 
-    const filterProduct =(cat) => {
-        const updatedList = data.filter((x)=>x.category === cat);
+    const filterProduct = (cat) => {
+        const updatedList = data.filter((x) => x.category === cat);
         setFilter(updatedList);
+        setCurrentPage(1); // reset current page to 1 when filter changes
     }
+
     const ShowProducts = () => {
+        const indexOfLastItem = currentPage * itemsPerPage;
+        const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+        const currentItems = filter.slice(indexOfFirstItem, indexOfLastItem);
+
         return (
             <>
                 <div className="buttons d-flex justify-content-center mb-5 pb-5">
-                    <button className="btn btn-outline-dark me-2" onClick={() =>setFilter(data)}>All</button>
-                    <button className="btn btn-outline-dark me-2" onClick={() =>filterProduct("men's clothing")}>Men's clothing</button>
-                    <button className="btn btn-outline-dark me-2" onClick={() =>filterProduct("women's clothing")}>Women's clothing</button>
-                    <button className="btn btn-outline-dark me-2" onClick={() =>filterProduct("jewelery")}>Jewellery</button>
-                    <button className="btn btn-outline-dark me-2" onClick={() =>filterProduct("electronics")}>Electronics</button>
+                    <button className="btn btn-outline-dark me-2" onClick={() => setFilter(data)}>All</button>
+                    <button className="btn btn-outline-dark me-2" onClick={() => filterProduct("men's clothing")}>Men's clothing</button>
+                    <button className="btn btn-outline-dark me-2" onClick={() => filterProduct("women's clothing")}>Women's clothing</button>
+                    <button className="btn btn-outline-dark me-2" onClick={() => filterProduct("jewelery")}>Jewellery</button>
+                    <button className="btn btn-outline-dark me-2" onClick={() => filterProduct("electronics")}>Electronics</button>
                 </div>
-                {filter.map((product) => {
-                    return (
-                        <>
-                            <div className="col-md-3 mb-4">
-                                <div class="card h-100 text-center p-4" key={product.id}>
-                                    <img src={product.image} class="card-img-top" alt={product.title} height="250px"/>
-                                        <div class="card-body">
-                                            <h5 class="card-title mb-0">{product.title.substring(0,12)}...</h5>
-                                            <p class="card-text lead fw-bold">
-                                                Ksh.{product.price}
-                                            </p>
-                                            <NavLink to={`/products/${product.id}`} className="btn btn-outline-dark">Buy Now</NavLink>
-                                        </div>
+                <div className="row">
+                    {currentItems.map((product) => (
+                        <div className="col-md-3 mb-4" key={product.id}>
+                            <div className="card h-100 text-center p-4">
+                                <img src={product.image} className="card-img-top" alt={product.title} height="250px" />
+                                <div className="card-body">
+                                    <h5 className="card-title mb-0">{product.title.substring(0, 12)}...</h5>
+                                    <p className="card-text lead fw-bold">
+                                        Ksh.{product.price}
+                                    </p>
+                                    <NavLink to={`/products/${product.id}`} className="btn btn-outline-dark">Buy Now</NavLink>
                                 </div>
                             </div>
-                        </>
-                    )
-                })}
+                        </div>
+                    ))}
+                </div>
+                <div className="pagination d-flex justify-content-center">
+                    {data.length > 0 &&
+                        Array(Math.ceil(filter.length / itemsPerPage))
+                            .fill()
+                            .map((_, index) => (
+                                <button
+                                    key={index}
+                                    className={currentPage === index + 1 ? 'active' : ''}
+                                    onClick={() => setCurrentPage(index + 1)}
+                                >
+                                    {index + 1}
+                                </button>
+                            ))}
+                </div>
+
             </>
         )
 
