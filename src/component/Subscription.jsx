6177f26df796eb8710f4968121c8316a208164fa
Whitney Madafali <65074238+WhitneyMadafali@users.noreply.github.com>
2023-02-21 20:23:11 +0300

    Spinner and Success message added

--- src/component/Subscription.jsx
+++ src/component/Subscription.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { FacebookShareButton, WhatsappShareButton, TelegramShareButton, TwitterShareButton } from 'react-share';
 import { FacebookIcon, WhatsappIcon, TelegramIcon, TwitterIcon } from 'react-share';
-const Subscription = () => {
+function Subscription() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+      setShowSuccessMessage(true);
+    }, 10000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
 
   return (
-    <div class="card">
-    <img src="/assets/musicbackground4.jpg" class="rounded mx-auto d-block" alt="background"></img>
-    <div class="d-flex justify-content-center">
+    <div className="container">
+      <div className="card">
+      <img src="/assets/m1.png" class="rounded mx-auto d-block" alt="background"></img>
+      {isLoading && (
+        
+        <div class="d-flex justify-content-center">
         <div class="spinner-border" role="status">
           <span class="visually-hidden">Loading...</span>
         </div>
       </div>
-      <div class="d-flex justify-content-center">
-      <p>Kindly wait as your subscription is being processed</p>
-      </div>
+      )}
+      {!isLoading && showSuccessMessage && (
+        <div class="d-flex justify-content-center">
+        <div className="success-message">
+          <p>Congratulations! You have successfully subscribed to this tune.</p>
+        </div>
+        </div>
+      )}
       <div class="d-flex justify-content-center">
         <FacebookShareButton
           url="https://skiza.africomltd.com/"
@@ -40,58 +61,10 @@ const Subscription = () => {
           <TwitterIcon logoFillColor="white" round={true}></TwitterIcon>
         </TwitterShareButton>
       </div>
-      </div>
-    );
-  };
-  
-  export default Subscription
-
-/*import React from 'react';
-import { FacebookShareButton, WhatsappShareButton, TelegramShareButton, TwitterShareButton } from 'react-share';
-import { FacebookIcon, WhatsappIcon, TelegramIcon, TwitterIcon } from 'react-share';
-
-const Subscription = () => {
-
-  return (
-    <div class="card">
-      <img src="..." class="text-center" alt="background" />
-      <div class="d-flex justify-content-center">
-        <div class="spinner-border" role="status">
-          <span class="visually-hidden">Loading...</span>
-        </div>
-      </div>
-
-      <div class="card-body justify-content-center">
-        <FacebookShareButton
-          url="https://skiza.africomltd.com/"
-          quote={"Enhance Your Calls with Skiza - The Music Caller Tune Subscription Service"}
-        >
-          <FacebookIcon logoFillColor="white" round={true}></FacebookIcon>
-        </FacebookShareButton>
-        <WhatsappShareButton
-          title={"Enhance Your Calls with Skiza - The Music Caller Tune Subscription Service"}
-          url="https://skiza.africomltd.com/"
-        >
-          <WhatsappIcon logoFillColor="white" round={true}></WhatsappIcon>
-        </WhatsappShareButton>
-        <TelegramShareButton
-          title={"Enhance Your Calls with Skiza - The Music Caller Tune Subscription Service"}
-          url="https://skiza.africomltd.com/"
-        >
-          <TelegramIcon logoFillColor="white" round={true}></TelegramIcon>
-        </TelegramShareButton>
-        <TwitterShareButton
-          title={"Enhance Your Calls with Skiza - The Music Caller Tune Subscription Service"}
-          url="https://skiza.africomltd.com/"
-        >
-          <TwitterIcon logoFillColor="white" round={true}></TwitterIcon>
-        </TwitterShareButton>
       </div>
     </div>
-
-
-
+    
   );
-};
+}
 
-export default Subscription*/
+export default Subscription
