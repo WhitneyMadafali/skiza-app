d78ebaec1306c0dcfad6390fc796429cbd9e24c2
Whitney Madafali <65074238+WhitneyMadafali@users.noreply.github.com>
2023-02-22 15:34:31 +0300

    Update Allignment

--- src/component/Subscription.jsx
+++ src/component/Subscription.jsx
@@ -18,52 +18,58 @@ function Subscription() {
 
   return (
     <div className="container">
-      <div className="card">
-      <img src="/assets/m1.png" class="rounded mx-auto d-block" alt="background"></img>
-      {isLoading && (
+      <div className="card" >
+        <img src="/assets/m5.jpg" class="rounded mx-auto d-block" alt="background"></img>
+        {isLoading && (
+
+          <div class="d-flex justify-content-center">
+            <div class="spinner-border" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+          </div>
+        )}
+        {!isLoading && showSuccessMessage && (
+          <div class="d-flex justify-content-center">
+          <div className="success-message">
+            <p style={{fontWeight: 'bold'}}>Congratulations! You have successfully subscribed to skiza tune.</p>
+          </div>
+        </div>
         
+        )}
         <div class="d-flex justify-content-center">
-        <div class="spinner-border" role="status">
-          <span class="visually-hidden">Loading...</span>
+          <FacebookShareButton
+            url="https://skiza.africomltd.com/"
+            quote={"Enhance Your Calls with Skiza - The Music Caller Tune Subscription Service"}
+            className="mx-3"
+          >
+            <FacebookIcon logoFillColor="white" round={true}></FacebookIcon>
+          </FacebookShareButton>
+          <WhatsappShareButton
+            title={"Enhance Your Calls with Skiza - The Music Caller Tune Subscription Service"}
+            url="https://skiza.africomltd.com/"
+            className="mx-3"
+          >
+            <WhatsappIcon logoFillColor="white" round={true}></WhatsappIcon>
+          </WhatsappShareButton>
+          <TelegramShareButton
+            title={"Enhance Your Calls with Skiza - The Music Caller Tune Subscription Service"}
+            url="https://skiza.africomltd.com/"
+            className="mx-3"
+          >
+            <TelegramIcon logoFillColor="white" round={true}></TelegramIcon>
+          </TelegramShareButton>
+          <TwitterShareButton
+            title={"Enhance Your Calls with Skiza - The Music Caller Tune Subscription Service"}
+            url="https://skiza.africomltd.com/"
+            className="mx-3"
+          >
+            <TwitterIcon logoFillColor="white" round={true}></TwitterIcon>
+          </TwitterShareButton>
         </div>
-      </div>
-      )}
-      {!isLoading && showSuccessMessage && (
-        <div class="d-flex justify-content-center">
-        <div className="success-message">
-          <p>Congratulations! You have successfully subscribed to this tune.</p>
-        </div>
-        </div>
-      )}
-      <div class="d-flex justify-content-center">
-        <FacebookShareButton
-          url="https://skiza.africomltd.com/"
-          quote={"Enhance Your Calls with Skiza - The Music Caller Tune Subscription Service"}
-        >
-          <FacebookIcon logoFillColor="white" round={true}></FacebookIcon>
-        </FacebookShareButton>
-        <WhatsappShareButton
-          title={"Enhance Your Calls with Skiza - The Music Caller Tune Subscription Service"}
-          url="https://skiza.africomltd.com/"
-        >
-          <WhatsappIcon logoFillColor="white" round={true}></WhatsappIcon>
-        </WhatsappShareButton>
-        <TelegramShareButton
-          title={"Enhance Your Calls with Skiza - The Music Caller Tune Subscription Service"}
-          url="https://skiza.africomltd.com/"
-        >
-          <TelegramIcon logoFillColor="white" round={true}></TelegramIcon>
-        </TelegramShareButton>
-        <TwitterShareButton
-          title={"Enhance Your Calls with Skiza - The Music Caller Tune Subscription Service"}
-          url="https://skiza.africomltd.com/"
-        >
-          <TwitterIcon logoFillColor="white" round={true}></TwitterIcon>
-        </TwitterShareButton>
-      </div>
+
       </div>
     </div>
-    
+
   );
 }
 
